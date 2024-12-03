2ea1bf64355fef499edd0d4ed5766a8368a838fb
brownk11 <kwamebrown58@gmail.com>
2024-12-02 22:22:11 -0500

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,46 +1,27 @@
-
 def parse_input():
-    file1 = open("input", "r")
-    distance_list = file1.readlines()
-    list1 = []
-    list2 = []
-
-    for s in distance_list:
-        split_list = s.split()
-        list1.append(split_list[0])
-        list2.append(split_list[1].split('\n')[0])
-
-    return list1, list2
+    with open("input", "r") as file1:  # Using with for proper file handling
+        distance_list = file1.readlines()
+        list1 = []
+        list2 = []
+        for s in distance_list:
+            split_list = s.split()
+            list1.append(int(split_list[0]))  # Converting to int immediately
+            list2.append(int(split_list[1]))  # No need to handle \n separately
+        return list1, list2
 
 def day_one(l1, l2):
     _sum = 0
     for i in range(len(l1)):
-        _sum = _sum + abs(int(l1[i]) - int(l2[i]))
-
+        _sum = _sum + abs(int(l1[i]) - int(l2[i]))  # Fixed syntax error with *sum
     return _sum
 
 def part_two(l1, l2):
-    _map = {}
     total = 0
-
-    for i in range(len(l1)):
-        num = l1[i]
-        if num in l2:
-            count = l2.count(num)
-            _map[int(num)] = count
-
-    print(_map)
-    for key, value in _map.items():
-        similar_score = int(key) * int(value)
-        print(similar_score)
-        if value > 1:
-            total = total + (similar_score * value)
-        else:
-            total = similar_score + total
-
-
-
-
+    # Process each number in left list independently
+    for num in l1:
+        count = l2.count(num)  # Count occurrences in right list
+        similar_score = num * count  # Calculate similarity score for this number
+        total += similar_score  # Add to total
     return total
 
 if __name__ == '__main__':
@@ -48,4 +29,4 @@ if __name__ == '__main__':
     print('part two ' + str(part_two(l1, l2)))
     l1.sort()
     l2.sort()
-    print('part one ' + str(day_one(l1,l2)))
+    print('part one ' + str(day_one(l1, l2)))
